7b052c0f0f4834fd0defba9d8f073dfee38c5e8b
Harinishri <shriharini41@gmail.com>
2026-07-15 11:54:28 +0530

    Time: 3 ms (57.43%) | Memory: 43.6 MB (22.12%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -4,10 +4,10 @@ class Solution {
         char[] text=tex.toCharArray();
         for(char c:text){
             if(c=='b') b++;
-            else if(c=='a') a++;
-            else if(c=='l') l++;
-            else if(c=='o') o++;
-            else if(c=='n') n++; 
+            else if(c=='a')a++;
+            else if(c=='l')l++;
+            else if(c=='o')o++;
+            else if(c=='n')n++; 
 
         }
         return Math.min(n,Math.min(Math.min(b,a),Math.min(l/2,o/2)));
