7fb2fa7fcf24ab927e274828ff0d7bfa29e0dfac
Harinishri <shriharini41@gmail.com>
2026-09-22 22:12:27 +0530

    Time: 3 ms (40.59%) | Memory: 42.8 MB (84.47%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count=0;
+        boolean inside=false;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='|'){
+                inside=!inside;
+            }
+            else if(s.charAt(i)== '*' && !inside){
+                count++;
+            }
+        }
+        return count;
+    }
+}
